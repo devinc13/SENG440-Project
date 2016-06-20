f14d7e2b4ce159347b2384c30d3bc5d588df2261
Devin Corrigall <Devin Corrigall>
2016-06-20 16:26:36 -0700

    Add given code for encoder

---
+++ encoder.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+
+char *HE_LUT[12] = {"0", "10", "110", "1110", "11110", "111110", "1111110", "11111110", "111111110", "1111111110", "11111111110", "11111111111"};
+
+int main(void) {
+char symbol_to_encode = 0;
+    do {
+        scanf("%i", &symbol_to_encode);
+	printf("%s\n", HE_LUT[symbol_to_encode - 0x40]);
+    } while ((symbol_to_encode > 0x40) & (symbol_to_encode < 0x46));
+    printf("%s\n", "Not a valid symbol.");
+}
+
