d3c31c38a26b9296c52bdc9d68e4d24ca8760544
Luke Mclaren <luke.patrick.mclaren@gmail.com>
2016-07-05 16:19:31 -0700

    Bunch of stuff

--- encoder.c
+++ encoder.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 
-char *HE_LUT[12] = {"0", "10", "110", "1110", "11110", "111110", "1111110", "11111110", "111111110", "1111111110", "11111111110", "11111111111"};
+char *HE_LUT[12] = {
+	"0",
+	"10",
+	"110",
+	"1110",
+	"11110",
+	"111110",
+	"1111110",
+	"11111110",
+	"111111110",
+	"1111111110",
+	"11111111110",
+	"11111111111"
+};
 
 int main(void) {
-char symbol_to_encode = 0;
-    do {
-        scanf("%i", &symbol_to_encode);
-	printf("%s\n", HE_LUT[symbol_to_encode - 0x40]);
-    } while ((symbol_to_encode > 0x40) & (symbol_to_encode < 0x46));
-    printf("%s\n", "Not a valid symbol.");
-}
+	int symbol_to_encode;
 
+	FILE *fp = fopen("./dataset.dat","r");
+
+	while(1){
+		symbol_to_encode = fgetc(fp);
+		if((feof(fp)) || (symbol_to_encode <= 0x60) || (symbol_to_encode >= 0x6D)){
+			break;
+		}
+
+		printf("%s", HE_LUT[symbol_to_encode - 97]);
+	}
+	printf("\n");
+}
